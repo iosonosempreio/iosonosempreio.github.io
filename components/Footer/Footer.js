13e696bbe5e234a6f708eff7117ba94b296b1c96
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-03-17 20:52:32 +0100

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<footer className={classNames(styles.footer, "my-3", "text-end")}>
-			<Link href="/">cv</Link> /{" "}
+			{/* <Link href="/">cv</Link> /{" "} */}
 			<Link href="https://twitter.com/iosonosempreio">tw</Link> /{" "}
 			<Link href="https://github.com/iosonosempreio">gh</Link> /{" "}
 			<Link href="https://observablehq.com/@iosonosempreio">hq</Link>
